e7bd2a5088142040b6105db966d49959b50814d0
Krysiah <Krysiah.Travis@laetottenham.ac.uk>
2024-12-24 15:11:58 +0000

    modifed player & item functions

--- item.py
+++ item.py
@@ -1,3 +1,19 @@
 class Item():
-    def __init__(self, name):
-        pass
+    def __init__(self, name, health_pts, sp_pts, weight, desc, can_use=False):
+        self.name = name
+        self.weight = 0
+        self.health_pts = health_pts
+        self.sp_pts = sp_pts
+        self.weight = weight
+        self.can_use = can_use
+        self.desc = desc
+
+    def give_item_desc(self):
+        print(self.name,"""
+""","[",self.desc,"]")
+
+#Items
+S_Heal_ptn = Item("Small Heal Potion",20,0,1,"A small healing potion. Restores 20 health",True)
+L_Heal_ptn = Item("Large Heal Potion",50,0,2,"A Large healing potion. Restores 50 health",True)
+
+sp_ptn = Item("Small SP Potion",0,20,1,"A small SP potion, Restores 20 SP",True)
--- player.py
+++ player.py
@@ -1,15 +1,15 @@
 class Player():
-    def __init__(self, given_name):
+    def __init__(self, given_name, skill):
         self.name = given_name
         self.health = 100
-        self.energy = 100
+        self.sp = 100
         self.inventory_max_weight = 50
         self.inventory = []
-        # add more atributes as needed
-
-    def calculate_inventory_size(self):
-        # write code here
-        pass
+        self.skill = skill
+    
+    def calculate_inventory_size(self,item_instance):
+        total_weight = sum(self.inventory(item_instance.weight))
+        return total_weight
 
     def add_item(self, item_instance):
         if self.calculate_inventory_size() > self.inventory_max_weight:
@@ -18,10 +18,12 @@ class Player():
             print("Your inventory is full...")
 
     def use_item(self, item_instance):
-        if item_instance.type == "food":
-            self.energy += 50
-        elif item_instance.type == "medicine":
-            self.health += 50
-        # add more code here
+        if item_instance.can_use == True:
+            self.health = self.health + item_instance.health_pts
+            self.sp = self.sp + item_instance.health_pts
+        else:
+            print("This item cannot be used")
+        
+
 
     # add more methods as needed
