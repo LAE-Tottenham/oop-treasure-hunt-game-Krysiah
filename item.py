163c19419fecd238a287024b5d0a3262daa72d8d
Krysiah <Krysiah.Travis@laetottenham.ac.uk>
2025-01-09 09:27:46 +0000

    trying to fix add item in player

--- item.py
+++ item.py
@@ -1,8 +1,9 @@
 class Item:
-    def __init__(self, name,can_use,desc):
+    def __init__(self, name,type,desc,weight):
         self.name = name
-        self.can_use = can_use
+        self.type = type
         self.desc = desc
+        self.weight = weight
 
     def give_item_desc(self):
         print(self.name,"""
@@ -11,23 +12,23 @@ class Item:
 #Battle Items
 class Battle_Item(Item):
     def __init__(self, name,type,desc,health_pts,sp_pts,weight):
-        super().__init__(name,type,desc)
+        super().__init__(name,type,desc,weight)
         self.health_pts = health_pts
         self.sp_pts = sp_pts
         self.weight = weight
 
  #Weapons   
 class Weapon(Item):
-    def __init__(self, name, type, hp_dmg, desc):
-        super().__init__(name, type, desc)
+    def __init__(self, name, type, hp_dmg, desc,weight):
+        super().__init__(name, type, desc,weight)
         self.hp_dmg = hp_dmg
 
 #Key Items
 class Key_Item(Item):
-    def __init__(self, name, type, desc):
-        super().__init__(name, type, desc)
-        self.can_use = False
+    def __init__(self, name, type, desc,weight,can_use=False):
+        super().__init__(name,type,weight,desc)
+        self.can_use = can_use
 
-sword = Weapon("Basic Sword","weapon",10,"A sturdy (and lame looking) sword. Nothing special, but good at slaying enemies.")
-hands = Weapon("these Hands!","weapon",16,"You pray that your enemies like Pokemon, cause they're gonna Catch 'Em All!")
-claw = Weapon("Scratch",True,"weapon","A simple scratch. Doesn't hurt much, but it's the attack that counts...")
+sword = Weapon("Basic Sword","weapon",10,"A sturdy (and lame looking) sword. Nothing special, but good at slaying enemies.",10)
+hands = Weapon("These Hands!","weapon",16,"You pray that your enemies like Pokemon, cause they're gonna Catch 'Em All!",0)
+claw = Weapon("Scratch","weapon",4,"A simple scratch. Doesn't hurt much, but it's the attack that counts...",0)
--- player.py
+++ player.py
@@ -1,6 +1,7 @@
-from item import Weapon
+from item import Weapon, Battle_Item
+import time
 
-hands = Weapon("these Hands!",True,16,"You pray that your enemies like Pokemon, cause they're gonna Catch 'Em All!")
+hands = Weapon("these Hands!",True,16,"You pray that your enemies like Pokemon, cause they're gonna Catch 'Em All!",10)
 
 class Player:
     def __init__(self, given_name):
@@ -11,23 +12,54 @@ class Player:
         self.max_sp = 70
         self.inventory_max_weight = 50
         self.inventory = []
+        self.weight = 0
         
-        self.hp_dmg = hands.hp_dmg
-
-    def calculate_inventory_size(self,item_instance):
-        total_weight = sum(self.inventory(item_instance.weight))
-        return total_weight
+        self.hp_dmg = None
+    
+    def check_inventory(self):
+        print("Here are your current inventory items:")
+        time.sleep(1)
+        print(self.inventory)
 
     def add_item(self, item_instance):
-        if self.calculate_inventory_size() > self.inventory_max_weight:
+        for item in self.inventory:
+            self.weight = item_instance.weight(item) 
+        if self.weight < self.inventory_max_weight:
             self.inventory.append(item_instance)
+            print("[Item added to the bag]")
+            time.sleep(1)
         else:
             print("Your inventory is full...")
+            time.sleep(1)
 
     def use_item(self, item_instance):
-        if item_instance.can_use == True:
+        #battle potions
+        if item_instance.type == "consume":
             self.health = self.health + item_instance.health_pts
-            self.sp = self.sp + item_instance.health_pts
+            self.sp = self.sp + item_instance.sp_pts
+            print("[Restored",item_instance.health_pts,"HP!]")
+            time.sleep(2)
+            print("[Restored",item_instance.sp_pts,"SP!]")
+            time.sleep(2)
+        #potions
+        elif item_instance.type == "weapon":
+            self.hp_dmg = item_instance.hp_dmg
+        #key items
+
+        #else
         else:
             print("This item cannot be used")
+            time.sleep(1)
         
+
+
+tst = Player("hero")
+
+print(tst.weight)
+
+tst.add_item(Battle_Item("S HP Potion","consume","tst potion 1",10,0,3))
+#tst.add_item(Battle_Item("L HP Potion","consume","tst potion 2",20,0,5))
+#tst.add_item(Battle_Item("S SP Potion","consume","tst potion 3",0,15,4))
+#tst.add_item(Battle_Item("L SP Potion","consume","tst potion 4",0,15,8))
+#tst.add_item(Battle_Item("Heavy item","consume","tst potion 5",0,15,10))
+#tst.add_item(Battle_Item("Can't fit item in","consume","tst potion 6",0,0,1))
